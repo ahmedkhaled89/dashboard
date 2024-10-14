1b0db6069485a81fa7dffa1d54d31e9acacd0f4c
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-10-14 22:40:49 +0300

    feat: add setting tooltip button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { FiSettings } from 'react-icons/fi';
 
 const App = () => {
-  return <div className='underline text-3xl'>App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className='flex relative dark:bg-main-dark-bg'>
+          <div className='fixed right-4 bottom-4' style={{ zIndex: 1000 }}>
+            <TooltipComponent content='setting' position='top'>
+              <button
+                type='button'
+                className='text-3xl p-3
+                hover:drop-shadow-xl
+              hover:bg-light-gray
+              text-white'
+                style={{ background: 'blue', borderRadius: '50%' }}
+              >
+                <FiSettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
