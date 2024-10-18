7bafc1323c5dd4513a5213f999f137b07f5039ae
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-10-19 01:08:39 +0300

    feat: add NavButton component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,25 @@ import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import avatar from '../data/avatar.jpg';
 import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => {
+  return (
+    <TooltipComponent content={title} position='BottomCenter'>
+      <button
+        type='button'
+        onClick={customFunc}
+        style={{ color }}
+        className='relative text-xl rounded-full p-3 hover:bg-light-gray'
+      >
+        <span
+          style={{ background: dotColor }}
+          className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2'
+        >
+          {icon}
+        </span>
+      </button>
+    </TooltipComponent>
+  );
+};
 const NavBar = () => {
   const { activeMenu, setActiveMenu } = useStateContext();
   return <div>NavBar</div>;
