a8e0ff28f7c1a0f887b80d24832a4c5e2b01fc60
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-10-19 00:59:18 +0300

    fix: fix classNames in Navbar parent div

--- src/App.js
+++ src/App.js
@@ -55,8 +55,8 @@ const App = () => {
             </div>
           )}
           <div
-            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
-              activeMenu ? 'md:ml-7' : 'flex-2'
+            className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full ${
+              activeMenu ? 'md:ml-72' : 'flex-2'
             }`}
           >
             <div className='fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full'>
